2120dc8a2c4da5f7fc41b95d7703c7afcc20ba5d
Michael Mei <40590072+michaelmei888@users.noreply.github.com>
2019-02-24 17:29:32 +1100

    Update installscrapy.sh

--- installscrapy.sh
+++ installscrapy.sh
@@ -16,26 +16,24 @@ scrapy startproject tutorial
 
 cd tutorial 
 
-import scrapy
-
 # >>>>> Create a spider in the project called "quotes"
-class QuotesSpider(scrapy.Spider):
-    name = "quotes"
-
-    def start_requests(self):
-        urls = [
-            'http://quotes.toscrape.com/page/1/',
-            'http://quotes.toscrape.com/page/2/',
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
-
-    def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename)
+#import scrapy
+#class QuotesSpider(scrapy.Spider):
+#    name = "quotes"
+#    def start_requests(self):
+#        urls = [
+#            'http://quotes.toscrape.com/page/1/',
+#            'http://quotes.toscrape.com/page/2/',
+#        ]
+#        for url in urls:
+#            yield scrapy.Request(url=url, callback=self.parse)
+#
+#    def parse(self, response):
+#        page = response.url.split("/")[-2]
+#        filename = 'quotes-%s.html' % page
+#        with open(filename, 'wb') as f:
+#            f.write(response.body)
+#        self.log('Saved file %s' % filename)
 # <<<<< End of the file
 
 #Run the spider
