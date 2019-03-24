b5af49d6a1b4f780a45e24221ca92aeb41368db5
Michael Mei <40590072+michaelmei888@users.noreply.github.com>
2019-03-24 21:26:54 +1100

    Update install_python3_ubuntu.sh

--- install_python3_ubuntu.sh
+++ install_python3_ubuntu.sh
@@ -1,10 +1,8 @@
 #If you are using Ubuntu 16.10 or newer, then you can easily install Python 3.6 with the following commands:
-
 sudo apt-get update
 sudo apt-get install python3.6
 
 #If you’re using another version of Ubuntu (e.g. the latest LTS release), we recommend using the deadsnakes PPA to install Python 3.6:
-
 sudo apt-get install software-properties-common
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt-get update
@@ -12,8 +10,10 @@ sudo apt-get install python3.6
 
 #If you are using other Linux distribution, chances are you already have Python 3 pre-installed as well. 
 #If not, use your distribution’s package manager. For example on Fedora, you would use dnf:
-
 sudo dnf install python3
+
+#install pip
+apt install python-pip
 
 #verify
 python3 --version
